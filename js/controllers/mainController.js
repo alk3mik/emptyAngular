e368436c4abcd6b25ead64fb7a1fb50b4a72cf7b
alk3mik <mozz76@gmail.com>
2017-06-29 18:45:20 +0200

    Home page done

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,11 @@
 // MAIN CONTROLLER
 function mainController($scope) {
-    
+	var vm = this;
+	vm.title = 'Customers';
+	vm.customers = [
+    	{name: 'Haley'},
+    	{name: 'Ella'},
+    	{name: 'Landon'},
+    	{name: 'John'}
+    ];   
 }
